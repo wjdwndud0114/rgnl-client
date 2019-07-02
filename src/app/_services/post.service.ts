0c0efef72eae53fd2938985ac3b0437a198bd8c8
wjdwndud0114 <wjdwndud0114@yahoo.com>
2019-07-01 18:37:38 -0700

    Add CUD to post service

--- src/app/_services/post.service.ts
+++ src/app/_services/post.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from '@/_models';
+import { Post } from '@/_models';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -11,6 +11,14 @@ export class PostService {
     constructor (private http: HttpClient) { }
 
     create (title: string, content: string, id: number) {
-        return this.http.post<User>(`${environment.apiUrl}/posts`, { "title": title, "content": content, "userId": id });
+        return this.http.post<Post>(`${environment.apiUrl}/posts`, { "title": title, "content": content, "userId": id });
+    }
+
+    edit (postId: number, title: string, content: string) {
+        return this.http.put<Post>(`${environment.apiUrl}/posts`, { "id": postId, "title": title, "content": content });
+    }
+
+    delete (postId: number) {
+        return this.http.delete<Post>(`${environment.apiUrl}/posts/${postId}`);
     }
 }
