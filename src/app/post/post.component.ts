37c18aa1d36a865492032b9c58722b1643c1fc4a
wjdwndud0114 <wjdwndud0114@yahoo.com>
2019-07-01 19:11:45 -0700

    WIP post editing/delete capabilities

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '@/_models';
+import { PostService } from '@/_services';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 @Component({
     selector: 'app-post',
@@ -7,11 +10,49 @@ import { Post } from '@/_models';
     styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-    @Input() post;
+    @Input() post: Post;
 
-    constructor () { }
+    editForm: FormGroup;
+    isEditing: boolean = false;
+    loading: boolean = false;
+
+    constructor (
+        private formBuilder: FormBuilder,
+        private postService: PostService
+    ) { }
 
     ngOnInit () {
+        this.editForm = this.formBuilder.group({
+            title: ['', Validators.required],
+            content: ['', Validators.required]
+        });
     }
 
+    get f() { return this.editForm.controls; }
+
+    saveChanges () {
+        if (this.editForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.postService.edit(this.post.Id, this.f.title.value, this.f.content.value)
+        .pipe(first())
+        .subscribe(
+            data => {
+                this.loading = false;
+            },
+            error => {
+                this.loading = false;
+            }
+        );
+    }
+
+    cancel () {
+
+    }
+
+    delete () {
+        this.postService.delete(this.post.Id);
+    }
 }
